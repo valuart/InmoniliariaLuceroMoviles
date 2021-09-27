b93447f5250494f3415347e477b4b553eeb0529e
valuart <42857715+valuart@users.noreply.github.com>
2021-09-27 13:30:59 -0300

    Segundo commit

---
+++ app/src/main/java/com/example/inmoniliarialuceromoviles/ui/inicio/InicioViewModel.java
@@ -0,0 +1,7 @@
+package com.example.inmoniliarialuceromoviles.ui.inicio;
+
+import androidx.lifecycle.ViewModel;
+
+public class InicioViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/inmoniliarialuceromoviles/ui/inicio/MapaFragment.java
@@ -0,0 +1,68 @@
+package com.example.inmoniliarialuceromoviles.ui.inicio;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.inmoniliarialuceromoviles.R;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapaFragment extends Fragment {
+
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            LatLng Inmobiliaria = new LatLng(-33.2727206, 66.3255049);
+            CameraPosition camPos = new CameraPosition.Builder()
+                    .target(Inmobiliaria)
+                    .zoom(70)
+                    .bearing(45)
+                    .tilt(90)
+                    .build();
+            CameraUpdate camUpd = CameraUpdateFactory.newCameraPosition(camPos);
+            googleMap.addMarker(new MarkerOptions().position(Inmobiliaria).title("Inmobiliaria Lucero"));
+            //googleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+            googleMap.animateCamera(camUpd);
+        }
+    };
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_mapa, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
+    }
+}
